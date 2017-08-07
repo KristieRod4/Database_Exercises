3829d15a2b9fd47d670e56a1cda7b5113fc72235
Kristie Rodriguez <pocketkristi@hotmail.com>
2017-08-07 17:01:57 -0500

    Adding MySQL exercises.

--- albums_migration1.sql
+++ albums_migration1.sql
@@ -5,8 +5,8 @@ DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist_first_name VARCHAR(50),
-    artist_last_name VARCHAR(100) NOT NULL,
-    name VARCHAR(128),
+    artist_last_name  VARCHAR(100) NOT NULL,
+    name VARCHAR (128)
     release_date DATE,
     sales DECIMAL(10,2),
     genre VARCHAR(150) NOT NULL,
---
+++ albums_seeder.sql
@@ -0,0 +1,40 @@
+USE codeup_test_db;
+
+INSERT INTO albums('artist_first_name', 'artist_last_name', 'name', 'release_date', 'sales', 'genre')
+VALUES ('Michael', 'Jackson', 'Thriller', '1982', '47.3', 'POP, Rock, R&B');
+
+INSERT INTO albums('artist_first_name', 'artist_last_name', 'name', 'release_date', 'sales', 'genre')
+VALUES	
+('Michael','Jackson','Thriller','1982',47000000.30, 'Pop', 'Rock, R&B'),
+('AC/DC','AC/DC', 'Back in Black', '1980', 26000000.10,	'Hard rock'),	
+('Pink', 'Floyd', 'The Dark Side of the Moon','1973', 24000000.20, 'Progressive rock'),
+('Whitney', 'Houston', 'The Bodyguard', '1992', 27000000.40, 'R&B, Soul, Pop, Soundtrack'),	
+('Meat', 'Loaf', 'Bat Out of Hell', '1977', 21000000.50, 'Hard rock, Progressive rock'),	
+('The', 'Eagles', 'Their Greatest Hits (1971–1975)', '1976', 32000000.20, 'Rock, Soft Rock, Folk Rock'),	
+('Bee', 'Gees', 'Saturday Night Fever', '1977', 20000000.60,'Disco'),	
+('Fleetwood', 'Mac', 'Rumours',	'1977', 27000000.90,'Soft rock'),	
+('Shania', 'Twain', 'Come On Over', '1997', 29600000, 'Country, pop'),
+('Led', 'Zeppelin', 'Led Zeppelin IV', '1971', 29000000, 'Hard rock, heavy metal'),
+('Michael', 'Jackson', 'Bad', '1987', 20300000, 'Pop, funk, rock'),
+('Alanis', 'Morissette', 'Jagged Little Pill', '1995', 24800000, 'Alternative rock'),
+('Celine', 'Dion', 'Falling into You', '1996', 20200000, 'Pop, Soft rock'),
+('The', 'Beatles', 'Sgt. Peppers Lonely Hearts Club Band', '1967', 13100000, 'Rock'),
+('The', 'Eagles', 'Hotel California', '1976', 21500000, 'Rock, soft rock, folk rock'),
+('Mariah', 'Carey', 'Music Box', '1993', 19000000, 'Pop, R&B, Rock'),
+('Michael', 'Jackson', 'Dangerous', '1991', 17600000, 'Rock, Funk, Pop'),
+('Various', 'Artists', 'Dirty Dancing', '1987', 17900000, 'Pop, rock, R&B'),
+('Celine', 'Dion', 'Lets Talk About Love', '1997', 19300000, 'Pop, Soft rock'),
+('The', 'Beatles', '1', '2000', 21600000, 'Rock'),
+('Adele', '21', '2011', 22300000, 'Pop, soul'),
+('The', 'Beatles', 'Abbey Road', '1969', 14400000, 'Rock'),
+('Bruce', 'Springsteen', 'Born in the U.S.A.', '1984', 19600000, 'Rock'),
+('Dire', 'Straits', 'Brothers in Arms', '1985', 17700000, 'Rock'),
+('James', 'Horner', 'Titanic: Music from the Motion Picture', '1997', 18100000, 'Soundtrack'),
+('Madonna', 'The Immaculate Collection', '1990', 19400000, 'Pop, Dance'),
+('Metallica', 'Metallica', '1991', 19900000, 'Thrash metal, heavy metal'),
+('Nirvana', 'Nevermind', '1991', 16700000, 'Grunge, alternative rock'),
+('Pink', 'Floyd', 'The Wall', '1979', 17600000, 'Progressive rock'),
+('Santana', 'Supernatural', '1999', 20500000, 'Rock'),
+('Guns N Roses', 'Appetite for Destruction', '1987', 21300000, 'Heavy metal, hard rock');
+
+
